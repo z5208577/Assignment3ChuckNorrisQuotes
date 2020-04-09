8c2b12c2f58c51eaf6bb593b88986c27b4a57c84
Steven Chung <z5208577@ad.unsw.edu.au>
2020-04-09 19:02:46 +1000

    Initial commit

---
+++ app/src/main/java/com/example/assignment3chuck/CategoryAdapter.java
@@ -0,0 +1,98 @@
+package com.example.assignment3chuck;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.assignment3chuck.Quote;
+import com.example.assignment3chuck.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+
+    private List<String> mCategories;
+    private TextView mQuoteText;
+    private Context mContext;
+
+
+    public CategoryAdapter(Context context,List<String> categories,TextView quoteText) {
+        mCategories = categories;
+        mContext = context;
+        mQuoteText = quoteText;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_list,parent,false);
+        return new ViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        holder.category.setText(mCategories.get(position));
+        //click makes a similar api call to the one in main activity.
+        holder.category.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(final View view){
+                final RequestQueue requestQueueQuote = Volley.newRequestQueue(view.getContext());
+                final String categoriesAPI = "https://api.chucknorris.io/jokes/random?category="+ holder.category.getText();
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        TextView quoteText = view.findViewById(R.id.quote);
+                        Quote quote = new Gson().fromJson(response,Quote.class);
+                        mQuoteText.setText("\"" + quote.getValue() + "\"");
+                        String toastString = "New quote of category: " + quote.getCategories();
+                        //to prove proper quote object creation
+                        toastString = toastString.substring(0,toastString.indexOf(":")+2)+ toastString.substring(toastString.indexOf("[")+1,toastString.indexOf("]"));
+                        Toast.makeText(mContext,toastString , Toast.LENGTH_SHORT).show();
+                        requestQueueQuote.stop();
+                    }
+                };
+
+                Response.ErrorListener errorListener = new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse (VolleyError error) {
+                        System.out.println(error.toString());
+                    }
+                };
+                StringRequest stringRequestQuote = new StringRequest(Request.Method.GET, categoriesAPI, responseListener, errorListener);
+                requestQueueQuote.add(stringRequestQuote);
+            }
+        });
+    }
+    //below is basic adapter methods
+    @Override
+    public int getItemCount() {
+        return mCategories.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView category;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            category = itemView.findViewById(R.id.category);
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/assignment3chuck/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.assignment3chuck;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    public TextView quoteText;
+    public Button newQuote;
+    public List<String> mCategories;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        quoteText = findViewById(R.id.quote);
+        newQuote=findViewById(R.id.newQuote);
+        //Implementation of the first core requirement: "a button to trigger the API call"
+        newQuote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //I prefer Volley as the parsing is manual (retorift request and response handling uses automatic parsing)
+                // and thus I can understand it better.
+                final RequestQueue requestQueueQuote = Volley.newRequestQueue(v.getContext());
+                final String quoteAPI = "https://api.chucknorris.io/jokes/random";
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Quote quote = new Gson().fromJson(response,Quote.class);
+                        quoteText.setText("\"" + quote.getValue() + "\"");
+                        requestQueueQuote.stop();
+                    }
+                };
+
+                Response.ErrorListener errorListener = new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse (VolleyError error) {
+                        System.out.println(error.toString());
+                    }
+                };
+                StringRequest stringRequestQuote = new StringRequest(Request.Method.GET, quoteAPI, responseListener, errorListener);
+                requestQueueQuote.add(stringRequestQuote);
+            }
+        });
+        //Additional functionality; random quote by category. Categories are displayed in a clickable recyclerview
+        getCategories(this.findViewById(android.R.id.content));
+    }
+    //
+    public void getCategories(View view){
+        final RequestQueue requestQueueQuote = Volley.newRequestQueue(view.getContext());
+        final String categoriesAPI = "https://api.chucknorris.io/jokes/categories";
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Type listType = new TypeToken<List<String>>() {}.getType();
+                mCategories = new Gson().fromJson(response, listType);
+                fillRecyclerView();
+                requestQueueQuote.stop();
+            }
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse (VolleyError error) {
+                System.out.println(error.toString());
+            }
+        };
+        StringRequest stringRequestQuote = new StringRequest(Request.Method.GET, categoriesAPI, responseListener, errorListener);
+        requestQueueQuote.add(stringRequestQuote);
+    }
+
+    //recycler view uses a grid layout.
+    public void fillRecyclerView(){
+        RecyclerView mRecyclerView = findViewById(R.id.categoryList);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this,3);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        //the quote textbox is passed in as it will need to change depending on the category clicked.
+        RecyclerView.Adapter mAdapter = new CategoryAdapter(this, mCategories,quoteText);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
